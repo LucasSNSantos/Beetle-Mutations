722c69f08f59decfbe52ddc5bd8136b015c74962
Lucas Souza <souzacontato18@gmail.com>
2022-04-08 21:32:42 -0300

    generating random beetles and control buttons

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,22 +21,62 @@ namespace BeetleGenetics
     public partial class MainWindow : Window
     {
         private NaturalSelection Darwin;
+        private List<Rectangle> MyBeetles;
+        public bool IsEvolving;
         public MainWindow()
         {
             InitializeComponent();
             Darwin = new NaturalSelection();
+            MyBeetles = new List<Rectangle>();
+            IsEvolving = false;
         }
 
         private void Generate_Population(object sender, RoutedEventArgs e)
         {
-            int individuals = 10;
-            var value = IndividualsNumber.SelectedValue;
-            if(value != null)
+            if (IsEvolving) return;
+            int individuals = 20;
+/*            var value = IndividualsNumber.SelectedValue;
+            if (value != null)
             {
                 var individualNumber = value.ToString();
                 individuals = int.Parse(individualNumber.Split()[1]);
-            }
-            Darwin.StartPopulation(individuals);
+            }*/
+            Setup(individuals);
         }
+
+        private void Clear_Population(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void Setup(int population)
+        {
+            Random R = new Random();
+            int height = 50;//R.Next(0, 800);
+            int width = 100;// R.Next(0,10);
+            Darwin.StartPopulation(population);
+            foreach (var item in Darwin.population.myPopulation)
+            {
+                var r = (byte) item.Red;
+                var g = (byte) item.Green;
+                var b = (byte) item.Blue;
+                var rect = new Rectangle()
+                {
+                    Fill = new SolidColorBrush(Color.FromArgb(255, r, g, b)),
+                    Height = 20,
+                    Width = 20,
+                    StrokeThickness = 2,
+                };
+                MyBeetles.Add(rect);
+                Canvas.SetLeft(rect, width);
+                Canvas.SetTop(rect, height);
+                myCanvas.Children.Add(rect);
+                width += 30;
+            }
+            IsEvolving = true;
+        }
+
+
     }
 }
